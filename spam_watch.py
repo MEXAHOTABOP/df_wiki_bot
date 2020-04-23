3a0943d4539baf2e79166a802ae7ac1c7d47fbd4
mexahotabop <mexahotabop@w1l.ru>
2020-04-23 15:34:56 +0300

    небольшой рефакторинг

--- spam_watch.py
+++ spam_watch.py
@@ -5,108 +5,111 @@ import pywikiapi.utils
 import os
 import re
 
-wiki = init_wiki(False)
-bot_token = os.environ["BOT_TOKEN"]
-channel_text_id = os.environ["CHANNEL_ID"]
-client = discord.Client()
-channel_text = ""
-timestamp = "20010115145600"
-
 # проверка известных паттернов спамботов
 # WwordNNWword WwordNN Wordd 
 mainspace_spambots = re.compile("(?:[a-zA-Z]+[0-9]{1,2}[a-zA-Z]+|[a-zA-Z]+[0-9]{1,2}|[A-Z][a-z]{4,9})")
-# Woooooooord
-userspace_spambots = re.compile("[A-Z][a-z]{9,13}")
+# Woooooooord WooordNNNN
+userspace_spambots = re.compile("(?:[A-Z][a-z]{9,13}|[A-Z][a-z]{4,8}[0-9]{4})")
 
 
-def check_nickname(name, pagename):
-    global mainspace_spambots
-    global userspace_spambots
+class SpamWatch(discord.Client):
+    def __init__(self):
+        super().__init__()
+        self.timestamp = "20010115145600"
+        self.wiki = init_wiki(False)
+        self.channel_text = self.get_channel(int(os.environ["CHANNEL_ID"]))
+        self.main_loop = self.loop.create_task(self.main_loop())
 
-    if mainspace_spambots.fullmatch(name) is not None or \
-            userspace_spambots.fullmatch(name) and "Участник:" in pagename:
-        return True
-    return False
+    def check_nickname(self, name, page_name):
+        global mainspace_spambots
+        global userspace_spambots
 
+        if mainspace_spambots.fullmatch(name) is not None or \
+                userspace_spambots.fullmatch(name) and "Участник:" in page_name:
+            return True
+        return False
 
-def is_first_action(name):
-    try:
-        change_list = wiki("query", list="usercontribs", ucuser=name, ucprop="sizediff")
-    except (TypeError, pywikiapi.utils.ApiError) as ex:
-        print(ex)
-        return is_first_action(name)
-
-    if len(change_list["query"]["usercontribs"]) == 1:
-        return True
-    return False
-
-
-def get_log():
-    global timestamp
-
-    try:
-        return wiki("query", list="recentchanges", rcend=timestamp,
-                    rcshow="!bot", rcprop="user|title|timestamp", rctype="new|edit", rclimit=50)
-    except Exception as ex:  # слишком много исключений потенциально могут быть
+    def is_less_then_five_actions(self, name):
         try:
+            change_list = self.wiki("query", list="usercontribs", ucuser=name, ucprop="sizediff")
+        except (TypeError, pywikiapi.utils.ApiError) as ex:
             print(ex)
-        except TypeError:
-            print("pywikiapi ex")
-        return None
+            return self.is_less_then_five_actions(name)
+
+        if len(change_list["query"]["usercontribs"]) < 5:
+            return True
+        return False
+
+    def get_log(self):
+        try:
+            return self.wiki("query", list="recentchanges", rcend=self.timestamp,
+                             rcshow="!bot", rcprop="user|title|timestamp", rctype="new|edit", rclimit=50)
+        except Exception as ex:  # слишком много исключений потенциально могут быть
+            try:
+                print(ex)
+            except TypeError:
+                print("pywikiapi ex")
+            return None
+
+    async def delete_page(self, title, nick, reason):
+        reason_list = {
+            1: "создание страниц без аккаунта запрещено",
+            2: "создание новой страницы с подозрительным никнеймом"
+        }
+        page = self.wiki("parse", page=title, prop="wikitext")["parse"]["wikitext"].replace("http", "")
+        try:
+            self.wiki("delete", title=title, reason=reason_list[reason], token=self.wiki.token(), POST=True)
+        except pywikiapi.utils.ApiError as ex:
+            if ex.data["code"] == "badtoken":
+                await asyncio.sleep(2)
+                self.wiki = init_wiki(False)
+                return self.delete_page(title, nick, reason)
+        msg = "**удалено:** `" + title + "` **автор**: <http://dfwk.ru/Служебная:Заблокировать/" + nick + \
+              "> **причина:** `" + reason_list[reason] + "` **содержание:**" + page[:100] + "..."
+        await self.send_message(msg)
+
+    async def send_message(self, msg):
+        try:
+            await self.channel_text.send(msg)
+        except Exception as e:
+            print(e)
+            await asyncio.sleep(2)
+            return self.send_message(msg)
+
+    async def main_loop(self):
+        await self.wait_until_ready()
+        while True:
+            await asyncio.sleep(10)
+            log_json = self.get_log()
+            if log_json is None:
+                continue
+            log_json = log_json["query"]["recentchanges"]
+
+            if len(log_json) and self.timestamp != log_json[0]["timestamp"]:
+                timestamp = log_json[0]["timestamp"]
+                for i in log_json:
+                    # новые страницы
+                    if i["type"] == "new":
+                        # пользователи без авторизации
+                        if "anon" in i:
+                            # print(i)
+                            await self.delete_page(i["title"], i["user"], 1)
+                            continue
+                        # спам боты по паттерну
+                        if self.check_nickname(i["user"], i["title"]) and self.is_less_then_five_actions(i["user"]):
+                            # print(i)
+                            await self.delete_page(i["title"], i["user"], 2)
+                            continue
 
 
-async def delete_page(title, nick, reason):
-    reason_list = {
-        1: "создание страниц без аккаунта запрещено",
-        2: "создание новой страницы с подозрительным никнеймом"
-    }
-    page = wiki("parse", page=title, prop="wikitext")["parse"]["wikitext"].replace("http", "")
-    wiki("delete", title=title, reason=reason_list[reason], token=wiki.token(), POST=True)
-    msg = "**удалено:** `" + title + "` **автор**: <http://dfwk.ru/Служебная:Заблокировать/" + nick + \
-          "> **причина:** `" + reason_list[reason] + "` **содержание:**" + page[:100] + "..."
-    await channel_text.send(msg)
+def run():
+    try:
+        client = SpamWatch()
+        client.run(os.environ["BOT_TOKEN"])
+    except Exception as ex:
+        print(ex)
+        asyncio.sleep(10)
+        return run()
 
 
-async def main_loop():
-    global timestamp
-    while True:
-        await asyncio.sleep(10)
-        log_json = get_log()
-        if log_json is None:
-            continue
-        log_json = log_json["query"]["recentchanges"]
-
-        if len(log_json) and timestamp != log_json[0]["timestamp"]:
-            timestamp = log_json[0]["timestamp"]
-            for i in log_json:
-                # новые страницы
-                if i["type"] == "new":
-                    # пользователи без авторизации
-                    if "anon" in i:
-                        # print(i)
-                        await delete_page(i["title"], i["user"], 1)
-                        continue
-                    # спам боты по паттерну
-                    if check_nickname(i["user"], i["title"]) and is_first_action(i["user"]):
-                        # print(i)
-                        await delete_page(i["title"], i["user"], 2)
-                        continue
-
-
-#  копипаста
-@client.event
-async def on_ready():
-    global client
-    global channel_text
-    global channel_text_id
-
-    print("ник {}".format(client.user.name))
-
-    # выбор текстового канала
-    channel_text = client.get_channel(int(channel_text_id))
-    print("текстовый канал {}".format(channel_text.name))
-
-    client.loop.create_task(main_loop())
-
-
-client.run(bot_token)
+run()
